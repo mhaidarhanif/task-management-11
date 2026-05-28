953d79e8f7ac614875d3eb78b6f5666ded80ad83
M Haidar Hanif <git@mhaidarhanif.com>
2026-05-28 22:46:26 +0700

    task card show isCompleted

--- src/app.tsx
+++ src/app.tsx
@@ -4,9 +4,27 @@ export function App() {
   return (
     <div>
       <h1>Task Management</h1>
-      <TaskCard task={{ title: "Breakfast", description: "Porridge" }} />
-      <TaskCard task={{ title: "Lunch", description: "Chicken soto" }} />
-      <TaskCard task={{ title: "Dinner", description: "Fried rice" }} />
+      <TaskCard
+        task={{
+          title: "Breakfast",
+          description: "Porridge",
+          isCompleted: true,
+        }}
+      />
+      <TaskCard
+        task={{
+          title: "Lunch",
+          description: "Chicken soto",
+          isCompleted: true,
+        }}
+      />
+      <TaskCard
+        task={{
+          title: "Dinner",
+          description: "Fried rice",
+          isCompleted: false,
+        }}
+      />
     </div>
   );
 }
--- src/task-card.tsx
+++ src/task-card.tsx
@@ -1,12 +1,15 @@
 type Task = {
   title: string;
   description: string;
+  isCompleted: boolean;
 };
 
 export function TaskCard({ task }: { task: Task }) {
   return (
     <div>
-      <h2>{task.title}</h2>
+      <h2>
+        {task.isCompleted && "✅"} {task.title}
+      </h2>
       <p>{task.description}</p>
     </div>
   );
