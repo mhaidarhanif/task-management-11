8af2fceef354ff6d96b90337763ba34330f43166
M Haidar Hanif <git@mhaidarhanif.com>
2026-05-28 22:06:36 +0700

    rename App to app

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-export function App() {
-  return (
-    <div>
-      <h1>Task Management</h1>
-    </div>
-  );
-}
---
+++ src/app_.tsx
@@ -0,0 +1,17 @@
+export function App() {
+  return (
+    <div>
+      <h1>Task Management</h1>
+      <TaskCard />
+    </div>
+  );
+}
+
+function TaskCard() {
+  return (
+    <div>
+      <h2>Dinner</h2>
+      <p>Fried rice</p>
+    </div>
+  );
+}
