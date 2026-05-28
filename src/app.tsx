ec815e657e6540fc4ce7727bcc1e5d105b340d17
M Haidar Hanif <git@mhaidarhanif.com>
2026-05-28 22:11:34 +0700

    task card component

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,10 @@
+import { TaskCard } from "./task-card";
+
 export function App() {
   return (
     <div>
       <h1>Task Management</h1>
       <TaskCard />
-    </div>
-  );
-}
-
-function TaskCard() {
-  return (
-    <div>
-      <h2>Dinner</h2>
-      <p>Fried rice</p>
     </div>
   );
 }
---
+++ src/task-card.tsx
@@ -0,0 +1,8 @@
+export function TaskCard() {
+  return (
+    <div>
+      <h2>Dinner</h2>
+      <p>Fried rice</p>
+    </div>
+  );
+}
