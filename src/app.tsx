e112f49673ebd721ddb64322548fd2a64354d1da
M Haidar Hanif <git@mhaidarhanif.com>
2026-06-07 11:48:18 +0700

    setup shadcn

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,23 @@
 import { TaskCard } from "./modules/task/components/task-card";
 import { dataTasks } from "./modules/task/task.data";
+import { Button } from "./components/ui/button";
 
 export function App() {
   return (
     <div>
-      <h1 className="bg-blue-200 text-5xl font-bold text-sky-800">
+      <h1 className="bg-blue-200 p-4 text-5xl font-bold text-sky-800">
         Task Management
       </h1>
 
-      {dataTasks.map((task) => {
-        return <TaskCard key={task.id} task={task} />;
-      })}
+      <ul className="flex flex-col gap-4 p-4">
+        {dataTasks.map((task) => {
+          return (
+            <li key={task.id}>
+              <TaskCard task={task} />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
--- src/modules/task/components/task-card.tsx
+++ src/modules/task/components/task-card.tsx
@@ -1,13 +1,29 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import type { Task } from "../task.type";
 import { TaskCheck } from "./task-check";
 
 export function TaskCard({ task }: { task: Task }) {
   return (
-    <div>
-      <h2>
-        <TaskCheck task={task} /> {task.title}
-      </h2>
-      <p>{task.description}</p>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>
+          <h2>
+            <TaskCheck task={task} /> {task.title}
+          </h2>
+        </CardTitle>
+      </CardHeader>
+
+      <CardContent>
+        <CardDescription>
+          <p>{task.description}</p>
+        </CardDescription>
+      </CardContent>
+    </Card>
   );
 }
