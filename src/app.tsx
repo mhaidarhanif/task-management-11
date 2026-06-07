842c47d479b86f607016dbbd7354ce9ca6f871d6
M Haidar Hanif <git@mhaidarhanif.com>
2026-06-07 10:33:33 +0700

    prettier sort tailwind

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,9 @@ import { dataTasks } from "./modules/task/task.data";
 export function App() {
   return (
     <div>
-      <h1 className="font-bold text-5xl">Task Management</h1>
+      <h1 className="bg-blue-200 text-5xl font-bold text-sky-800">
+        Task Management
+      </h1>
 
       {dataTasks.map((task) => {
         return <TaskCard key={task.id} task={task} />;
